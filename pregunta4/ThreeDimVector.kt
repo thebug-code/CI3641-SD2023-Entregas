e550d988810a4f40231ccd260fadc67972cb451c
admeneses <alejandrocardie12@gmail.com>
2023-10-15 14:24:45 -0400

    Agregar mas casos de prueba y fix bugs

--- pregunta4/ThreeDimVector.kt
+++ pregunta4/ThreeDimVector.kt
@@ -2,9 +2,14 @@
  * Definicion de vectores de tres coordenadas y de las operaciones suma, resta, 
  * productor cruz, producto punto y norma sobre estos.
  */
-package com.threedimvector
 
-data class Vector(val x: Int, val y: Int, val z: Int) {
+package com.threedimvector
+import kotlin.math.sqrt
+import java.text.DecimalFormat
+
+data class Vector(val x: Double, val y: Double, val z: Double) {
+    private val df = DecimalFormat("#.##")
+
     operator fun plus(other: Vector): Vector {
         return Vector(x + other.x, y + other.y, z + other.z)
     }
@@ -12,17 +17,18 @@ data class Vector(val x: Int, val y: Int, val z: Int) {
     operator fun minus(other: Vector): Vector {
         return Vector(x - other.x, y - other.y, z - other.z)
     }
-    
      
     operator fun times(other: Vector): Vector {
         return Vector(y * other.z - z * other.y, z * other.x - x * other.z, x * other.y - y * other.x)
     }
     
-    
     operator fun rem(other: Vector): Vector {
         return Vector(x * other.x, y * other.y, z * other.z)
     }
-    
+
+    val norm: Double
+        get() = df.format(sqrt(x * x + y * y + z * z)).toDouble()
+
     operator fun plus(n: Int): Vector {
         return Vector(x + n, y + n, z + n)
     }
@@ -35,11 +41,29 @@ data class Vector(val x: Int, val y: Int, val z: Int) {
         return Vector(x * n, y * n, z * n)
     }
 
+    operator fun plus(n: Double): Vector {
+        return Vector(df.format(x + n).toDouble(), df.format(y + n).toDouble(), df.format(z + n).toDouble())
+    }
+
+    operator fun minus(n: Double): Vector {
+        return Vector(df.format(x - n).toDouble(), df.format(y - n).toDouble(), df.format(z - n).toDouble())
+    }
+
+    operator fun rem(n: Double): Vector {
+        return Vector(df.format(x * n).toDouble(), df.format(y * n).toDouble(), df.format(z * n).toDouble())
+    }
+    
     override fun equals(other: Any?): Boolean {
         when(other) {
     	    is Vector -> {
                 return x == other.x && y == other.y && z == other.z
             } else -> return false
     	}
+    }
+
+    companion object {
+        operator fun invoke(x: Int, y: Int, z: Int): Vector {
+            return Vector(x.toDouble(), y.toDouble(), z.toDouble())
+        }
     }
 }
--- pregunta4/VectorTest.kt
+++ pregunta4/VectorTest.kt
@@ -38,14 +38,50 @@ internal class VectorTest {
     }
 
     @Test
+    fun vectorAddition3() {
+        val expected = Vector(4.22, 5.22, 12.22)
+        assertEquals(expected, v1 + 7.22)
+    }
+
+    @Test
     fun vectorSubtraction2() {
         val expected = Vector(-8, -7, 0)
         assertEquals(expected, v1 - 5)
     }
 
     @Test
+    fun vectorSubtraction3() {
+        val expected = Vector(-7.55, -6.55, 0.45)
+        assertEquals(expected, v1 - 4.55)
+    }
+
+    @Test
     fun vectorScalarMultiplication2() {
         val expected = Vector(-15, -10, 25)
         assertEquals(expected, v1 % 5)
+    }
+
+    @Test
+    fun vectorScalarMultiplication3() {
+        val expected = Vector(-6.0, -4.0, 10.0)
+        assertEquals(expected, v1 % 2.0)
+    }
+
+    @Test
+    fun vectorNorm() {
+        val expected = 6.16
+        assertEquals(expected, v1.norm)
+    }
+
+    @Test
+    fun expresion1() {
+        val expected = Vector(18.48, -73.92, 24.64)
+        assertEquals(expected, (v1 + v2) % v1.norm)
+    }
+
+    @Test
+    fun expresion() {
+        val expected = Vector(323.02, 186.02, 276.42)
+        assertEquals(expected, (v1 - v2) + (v1 * v2) % v1.norm + v2.norm)
     }
 }
