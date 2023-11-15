615bd95fbd34241a21d25abd41b1ecf8bdb5f3c7
admeneses <alejandrocardie12@gmail.com>
2023-11-15 02:01:07 -0400

    tail recursion

--- parcial2/pregunta4/function_family.cpp
+++ parcial2/pregunta4/function_family.cpp
@@ -5,22 +5,43 @@ using namespace std;
 // X = 5, Y = 3, Z = 6
 // alpha = 6, beta = 7
 
-int f_six_seven_recursion(int n) {
+// recursive
+int f_six_seven_recursive(int n) {
     if (n >= 0 && n < 42) {
         return n;
     } else if (n >= 42) {
-        return f_six_seven_recursion(n - 7) + f_six_seven_recursion(n - 14) \
-            + f_six_seven_recursion(n - 21) + f_six_seven_recursion(n - 28) \
-            + f_six_seven_recursion(n - 35) + f_six_seven_recursion(n - 42);
+        return f_six_seven_recursive(n - 7) + f_six_seven_recursive(n - 14) \
+            + f_six_seven_recursive(n - 21) + f_six_seven_recursive(n - 28) \
+            + f_six_seven_recursive(n - 35) + f_six_seven_recursive(n - 42);
     } else {
         throw invalid_argument("n must be non-negative");
     }
 }
 
+// tail recursion
+int f_six_seven_tail_recursion(int n, int a, int b, int c, int d, int e, int f) {
+    if (n >= 0 && n < 42) {
+        return n;
+    } else if (0 <= n - 7 && n - 7 < 42) {
+        return a + b + c + d + e + f;
+    } else if (n >= 42) {
+        return f_six_seven_tail_recursion(n - 7, b, c, d, e, f, a + b + c + d + e + f);
+    } else {
+        throw invalid_argument("n must be non-negative");
+    }
+}
+
+int f_six_seven_tail_recursion_aux(int n) {
+    int i = n % 7;
+    return f_six_seven_tail_recursion(n, i, i + 7, i + 14, i  + 21, i + 28, i + 35);
+}
+
+
 int main() {
     int n;
     cin >> n;
-    cout << f_six_seven_recursion(n) << endl;
+    cout << f_six_seven_recursive(n) << endl;
+    cout << f_six_seven_tail_recursion_aux(n) << endl;
     return 0;
 }
 
