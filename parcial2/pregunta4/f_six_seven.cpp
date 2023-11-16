712935e96b1d6f96cee02dc3c8776acd0307e0a5
admeneses <alejandrocardie12@gmail.com>
2023-11-15 23:14:51 -0400

    add plot

--- parcial2/pregunta4/f_six_seven.cpp
+++ parcial2/pregunta4/f_six_seven.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <vector>
 #include <stdexcept>
+#include <chrono>
+#include "matplotlibcpp.h"
+
 using namespace std;
+namespace plt = matplotlibcpp;
+using namespace std::chrono;
 
 // X = 5, Y = 3, Z = 6
 // alpha = 6, beta = 7
@@ -40,6 +46,7 @@ int f67_iterative(int n) {
     if (n >= 0 && n < 42) {
         return n;
     }
+
     int i = n % 7;
     int a = i, b = i + 7, c = i + 14, d = i + 21, e = i + 28, f = i + 35;
 
@@ -67,17 +74,45 @@ int f67_iterative(int n) {
 }
 
 int main() {
-    //int n;
-    //cin >> n;
-    //cout << f67_recursive(n) << endl;
-    //cout << f67_tail_recursion_aux(n) << endl;
-    //cout << f67_iterative(n) << endl;
+    vector<long> t1, t2, t3;
+    vector<int> ns;
 
-    for (int i = 0; i < 100; i++) {
-        //cout << (f67_recursive(i) == f67_tail_recursion_aux(i)) << endl;
-        cout << (f67_tail_recursion_aux(i) == f67_iterative(i)) << endl;
+    for (int i = 10; i < 250; i += 10) {
+        // f67_recursive
+        auto start = high_resolution_clock::now();
+        f67_recursive(i);
+        auto stop = high_resolution_clock::now();
+        auto d1 = duration_cast<microseconds>(stop - start);
+        t1.push_back(d1.count());
+        
+        // f67_tail_recursion
+        start = high_resolution_clock::now();
+        f67_tail_recursion_aux(i);
+        stop = high_resolution_clock::now();
+        auto d2 = duration_cast<microseconds>(stop - start);
+        t2.push_back(d2.count());
+
+        // f67_iterative
+        start = high_resolution_clock::now();
+        f67_iterative(i);
+        stop = high_resolution_clock::now();
+        auto d3 = duration_cast<microseconds>(stop - start);
+        t3.push_back(d3.count());
+
+        // Agrega los tiempos a un vector
+        ns.push_back(i);
     }
+
+    // Grafica los tiempos
+    plt::named_plot("f67_recursive", ns, t1, "r-");
+    plt::named_plot("f67_tail_recursion", ns, t2, "g-");
+    plt::named_plot("f67_iterative", ns, t3, "b-");
+
+    plt::title("f67");
+    plt::xlabel("n");
+    plt::ylabel("t (microseconds)");
+    plt::legend();
+    plt::show();
 
     return 0;
 }
-
