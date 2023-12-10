fc13047b79df3639df3012208cc220616a2da8f2
admeneses <alejandrocardie12@gmail.com>
2023-12-10 14:02:13 -0400

    examen 3 p1b1

---
+++ parcial3/pregunta1/secuencia.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic
+
+T = TypeVar('T')
+
+class Secuencia(ABC, Generic[T]):
+    def __init__(self):
+        self._secuencia = []
+
+    @abstractmethod
+    def agregar(self, elemento: T):
+        pass
+
+    @abstractmethod
+    def remover(self) -> T:
+        pass
+
+    def vacio(self) -> bool:
+        pass
